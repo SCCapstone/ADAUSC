62c4c4eb26d7288e53166abcf1289ed591ea5f5a
ParkerM <parkermauney@gmail.com>
2014-10-02 19:41:03 -0400

    trying initial commit

---
+++ src/com/example/adausc/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.adausc;
+
+import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+	
+	final Context context = this;
+	private Button button;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		button = (Button) findViewById(R.id.button1);
+		
+		//add button listener
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+				
+				//set title
+				alertDialogBuilder.setTitle("Test title");
+				
+				//set dialog message
+				alertDialogBuilder
+					.setMessage("Click \"Yes\" to exit")
+					.setCancelable(false)
+					.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int id) {
+							//if this button is clicked, close current activity
+							MainActivity.this.finish();
+						}
+					})
+					.setNegativeButton("No", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int id) {
+							//if this button is clicked, just close the dialog box
+							dialog.cancel();
+						}
+					});
+					
+					//create alert dialog
+					AlertDialog alertDialog = alertDialogBuilder.create();
+					
+					//show it
+					alertDialog.show();
+			}
+		});
+	}
+}
