14ffac353f06166c949385aca40fb5f21f778a1c
ParkerM <parkermauney@gmail.com>
2014-10-19 18:11:19 -0400

    home activity test

---
+++ app/src/main/java/edu/sc/cse/adausc/HomeActivity.java
@@ -0,0 +1,106 @@
+package edu.sc.cse.adausc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.DialogInterface;
+
+public class HomeActivity extends Activity {
+
+    private AlertDialog.Builder alert;
+    private Button btBrowse;
+    private Button btCalculator;
+    private Button btFavorites;
+    private Button btDocument;
+    private TextView aboutLink;
+    private TextView contactLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getActionBar().hide();
+
+        alert = new AlertDialog.Builder(HomeActivity.this);
+        alert.setTitle("About");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText(HomeActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText(HomeActivity.this, "Fail", Toast.LENGTH_SHORT) .show();
+            }
+        });
+        btBrowse = (Button) findViewById(R.id.buttonBrowse);
+        btCalculator = (Button) findViewById(R.id.buttonCalculator);
+        btFavorites = (Button) findViewById(R.id.buttonFavorites);
+        btDocument = (Button) findViewById(R.id.buttonDocument);
+
+        btBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //when this button is clicked, show the alert
+                alert.show();
+            }
+        });
+
+        btCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //when this button is clicked, show the alert
+                alert.show();
+            }
+        });
+
+        btFavorites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //when this button is clicked, show the alert
+                alert.show();
+            }
+        });
+
+        btDocument.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //when this button is clicked, show the alert
+                alert.show();
+            }
+        });
+
+        aboutLink = (TextView) findViewById(R.id.about);
+        aboutLink.setMovementMethod(LinkMovementMethod.getInstance());
+        contactLink = (TextView) findViewById(R.id.contact);
+        contactLink.setMovementMethod(LinkMovementMethod.getInstance());
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
