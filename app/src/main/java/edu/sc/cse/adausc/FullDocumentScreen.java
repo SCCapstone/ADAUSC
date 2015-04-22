74e992837187c9846c55307a4d6a31a9c0b433fc
dohertsm <dohertsm@email.sc.edu>
2015-04-22 12:33:54 -0400

    took out spinner
    cause i broke it...

--- app/src/main/java/edu/sc/cse/adausc/FullDocumentScreen.java
+++ app/src/main/java/edu/sc/cse/adausc/FullDocumentScreen.java
@@ -2,7 +2,6 @@ package edu.sc.cse.adausc;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
 
@@ -14,12 +13,10 @@ public class FullDocumentScreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        oSpinner = (ProgressBar)findViewById(R.id.docProgressBar);
-        oSpinner.setVisibility(View.GONE);
         setContentView(R.layout.activity_fulldoc);
         WebView oContent = (WebView) findViewById(R.id.fullDoc);
         oContent.loadData(SerializationHelper.LoadFullDoc(), "text/html", null);
-        oSpinner.setVisibility(View.GONE);
+
         //enable return to home from action bar
         getActionBar().setDisplayHomeAsUpEnabled(true);
     }
